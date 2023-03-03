8ca5e7efcdb5d231358f7b4fb43592df99d44b0d
DumboJ <dumbojxx@gmail.com>
2023-03-03 23:44:42 +0800

    ways of DestoryBean demo ：1.@PreDestory  2.implements DisposableBean.destroy() 3.自定义销毁方法

--- java_beans/src/main/java/cn/dumboj/beaninfo/instantiation/factory/PersonFactoryImpl.java
+++ java_beans/src/main/java/cn/dumboj/beaninfo/instantiation/factory/PersonFactoryImpl.java
@@ -1,10 +1,12 @@
 package cn.dumboj.beaninfo.instantiation.factory;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class PersonFactoryImpl implements PersonFactory, InitializingBean {
+public class PersonFactoryImpl implements PersonFactory, InitializingBean, DisposableBean {
     /**
      * 1.@PostContruct方式  初始化bean
      * */
@@ -24,5 +26,26 @@ public class PersonFactoryImpl implements PersonFactory, InitializingBean {
      * */
     public void definedSelfMethodInitialization() {
         System.out.println(" 自定义初始化方法的方式 初始化中...");
+    }
+    /**
+     * 1. @PreDestory  销毁Bean
+     *
+     * */
+    @PreDestroy
+    public void preDestoryBean() {
+        System.out.println("@PreDestroy 销毁bean : 销毁中...");
+    }
+    /**
+     * 2.DisposableBean#destroy() 销毁bean
+     * */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println(" DisposableBean#destroy(): 销毁中...");
+    }
+    /**
+     * 3.自定义销毁方法的方式
+     * */
+    public void doDestory() {
+        System.out.println(" 自定义销毁方法的方式 初始化中...");
     }
 }
--- java_beans/src/main/java/cn/dumboj/beaninfo/instantiation/init/InitializationBean.java
+++ java_beans/src/main/java/cn/dumboj/beaninfo/instantiation/init/InitializationBean.java
@@ -5,15 +5,26 @@ import cn.dumboj.beaninfo.instantiation.factory.PersonFactoryImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /**
- * Spring初始化Bean的方式（按存在于同一bean中执行顺序排列）
+ * todo
+ *  Spring初始化Bean的方式（按存在于同一bean中执行顺序排列）
  * 1.@PostConstruct 注解
  * 2.实现InitializingBean 接口中 afterPropertiesSet()方法
  * 3.自定义初始化方法
  *                 a:<bean init-method=""/>
  *                 b:@Bean(intiMethod = "")
  *                 c:JavaAPI AbstractBeanDefinition#setInitMethodName("")
+ *
+ *  todo --------------------------------------------------------------------------------
+ *   Spring销毁Bean的方式（按存在于同一bean中执行顺序排列）
+ *  * 1.@PreDestory 注解
+ *  * 2.实现DisposableBean 接口中 destroy()方法
+ *  * 3.自定义初始化方法
+ *  *                 a:<bean destroy-method=""/>
+ *  *                 b:@Bean(destroyMethod = "")
+ *  *                 c:JavaAPI AbstractBeanDefinition#setDestroyMethodName("")
  * */
 @Configuration
 public class InitializationBean {
@@ -29,7 +40,9 @@ public class InitializationBean {
         //关闭 应用上下文
         context.close();
     }
-    @Bean(initMethod = "definedSelfMethodInitialization")
+    @Bean(initMethod = "definedSelfMethodInitialization",destroyMethod = "doDestory")
+    //延时加载
+    @Lazy
     public PersonFactory initPersonFactory() {
         return new PersonFactoryImpl();
     }
