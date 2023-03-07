371dce518a94e2b3813852da39c4895958e9356d
DumboJ <dumbojxx@gmail.com>
2023-03-07 23:54:16 +0800

    register bean through out Singleton bean

---
+++ java_beans/src/main/java/cn/dumboj/beaninfo/register/SingletonBeanRegisterDemo.java
@@ -0,0 +1,11 @@
+package cn.dumboj.beaninfo.register;
+/**
+ *注册Spring Bean的方式 ，2.通过注册外部单体对象注册 bean
+ *
+ *                      1. 通过BeanDefinition 注册
+ * */
+public class SingletonBeanRegisterDemo {
+    public static void main(String[] args) {
+
+    }
+}
