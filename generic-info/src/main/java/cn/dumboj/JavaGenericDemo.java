d42230acc6d09da273e37ae428a4bd1af6931233
DumboJ <dumbojxx@gmail.com>
2025-01-24 23:39:13 +0800

    泛型模块内容知识

--- generic-info/src/main/java/cn/dumboj/JavaGenericDemo.java
+++ generic-info/src/main/java/cn/dumboj/JavaGenericDemo.java
@@ -1,6 +1,7 @@
 package cn.dumboj;
 /**
- * Java 泛型支持
+ * Java 泛型支持、
+ * {@link java.lang.reflect.GenericArrayType}
  * */
 public class JavaGenericDemo {
     public static void main(String[] args) {
